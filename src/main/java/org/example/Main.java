6f38284839ea8a16098e4bab26a37f98ff246c28
Akhil Razdan <akhilz@amazon.com>
2024-12-31 14:53:42 -0800

    ZooKeeper HelloWorld

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.io.IOException;
+
+import static org.apache.zookeeper.ZooDefs.Ids.OPEN_ACL_UNSAFE;
+
 public class Main {
-    public static void main(String[] args) {
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 15000, null);
+
+        // Create
+        zooKeeper.create("/node", "data".getBytes(), OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, null);
+        zooKeeper.create("/node/child", "child".getBytes(), OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, null);
+
+        // Read
+        Stat stat = new Stat();
+        var data = zooKeeper.getData("/node", false, stat);
+        LOG.info(new String(data));
+        LOG.info(String.valueOf("version = " + stat.getVersion()));
+
+        List<String> children = zooKeeper.getChildren("/node", false);
+        children.forEach(child -> LOG.info("child found = " + child));
+
+        // Update
+        zooKeeper.setData("/node", "new data".getBytes(), -1);
+        // Version = -1 means it updates the latest version
+
+        // Delete
+        zooKeeper.delete("/node/child", -1);
+        zooKeeper.delete("/node", -1);
+
         System.out.println("Hello world!");
     }
 }
